3cb581372c72b3378fc2b5c94b995956f7c1c9d4
Angel Luis Robles Fernandez <a.l.robles.fernandez@gmail.com>
2023-06-10 08:32:43 -0700

    finish get_mdd function. Get all orers

--- R/get_mdd.R
+++ R/get_mdd.R
@@ -8,42 +8,36 @@
 #'  three taxonomic authorities
 #'  <https://doi.org/10.1111/jbi.14330>
 #' @examples
-#' get_mdd("all")
-get_mdd <- function(Order = "all", dir = NULL){
-  if(Order == "all")
-    Order = sort(unique(mddSpList$Order))
+#' get_mdd()
+get_mdd <- function(dir = NULL){
 
-  if(
-    any(
-      is.na(
-        match(Order, unique(mddSpList$Order)))
-    )
-  ){
-    stop("Order does not exist! Check spell")
-  }
+  Order <- unique(mddSpList$Order)
+
   #### progress call
 
   map_mdd_progress <- function(.x = Order, ...) {
-    #.f <- purrr::as_mapper(.f, ...)
     pb <- progress::progress_bar$new(total = length(.x), format = " [:bar] :current/:total (:percent) eta: :eta", force = TRUE)
+
     f <- function(...) {
       pb$tick()
       get_mdd_order(...)
     }
     Map(f = f, .x )
   }
+
   root <- system.file(package = "mdd")
   filePath <- paste0(root, "/", "data/MDD.rds")
+
   if(!file.exists(filePath)){
     MapList <- map_mdd_progress(Order)
     shp <- terra::vect(MapList)
     shp <- terra::wrap(shp)
-    readr::write_rds(shp, file = filePath, compress = "xz")
+    readr::write_rds(x = shp, file = filePath, compress = "xz")
   } else{
     shp <- readr::read_rds(filePath)
   }
   if (!is.null(dir)) {
-    readr::write_rds(shp, file = paste0(dir, "/MDD_", Order, ".rds"), compress = "xz")
+    readr::write_rds(shp, file = paste0(dir, "/MDD.rds"), compress = "xz")
   }
   return(terra::unwrap(shp))
 }
